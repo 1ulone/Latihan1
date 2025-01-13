9343f135e1be4a46363be03774885daec541b6c8
RoeRu <bimo.17168@gmail.com>
2025-01-14 00:25:51 +0700

    Minor [RR]: Adding namespace in OnTouchInterface

--- Assets/Script/Environment/OnTouchInterface.cs
+++ Assets/Script/Environment/OnTouchInterface.cs
@@ -1,33 +1,36 @@
 using System;
 using UnityEngine;
 
-public class OnTouchInterface
+namespace InteractionSystem
 {
-    public enum InteractionType
+    public class OnTouchInterface
     {
-        Spike,
-        DBlock,
-        Goal
-    }
-    /// <summary>
-    /// Respon to different type of interaction that happens inside the game
-    /// </summary>
-    /// <param name="typeOfInteraction">Specify the type of the interaction</param>
-    public void Touch(InteractionType typeOfInteraction)
-    {
-        switch (typeOfInteraction)
+        public enum InteractionType
         {
-            case InteractionType.Spike:
-                Debug.Log("Dead!");
-                break;
+            Spike,
+            DBlock,
+            Goal
+        }
+        /// <summary>
+        /// Respon to different type of interaction that happens inside the game
+        /// </summary>
+        /// <param name="typeOfInteraction">Specify the type of the interaction</param>
+        public void Touch(InteractionType typeOfInteraction)
+        {
+            switch (typeOfInteraction)
+            {
+                case InteractionType.Spike:
+                    Debug.Log("Dead!");
+                    break;
 
-            case InteractionType.DBlock:
-                Debug.Log("Dissapar");
-                break;
+                case InteractionType.DBlock:
+                    Debug.Log("Dissapar");
+                    break;
 
-            case InteractionType.Goal:
-                Debug.Log("Finishhh!!!");
-                break;
+                case InteractionType.Goal:
+                    //LevelUp();
+                    break;
+            }
         }
     }
 }
